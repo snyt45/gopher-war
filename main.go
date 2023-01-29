5c67f5dcfd17a1e80d71613a23315f31d0525348
yuta.sano <snyt45@gmail.com>
2023-01-29 18:38:37 +0900

    get /ws

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/olahol/melody"
 )
 
 func main() {
 	r := gin.Default()
+	m := melody.New()
 
 	r.GET("/", func(c *gin.Context) {
 		http.ServeFile(c.Writer, c.Request, "static/index.html")
 	})
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
 	r.Run()
 }
